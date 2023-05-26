8d33b106f5bd894f8d3f8678412d99643591ed40
Scott Snyder <89752532+DesertedFuture@users.noreply.github.com>
2023-05-25 17:38:50 -0700

    Add files via upload

--- DataStructs/linkedList.cpp
+++ DataStructs/linkedList.cpp
@@ -27,47 +27,24 @@ struct Node{
 	int data;
 	struct Node* next;
 };
-Node * createNode(int val){
+Node* createNode(int val){
 	Node* temp = new Node();
 	temp->data = val;
 	temp->next = NULL;
 	return temp;
 }
 
-//given a node, insert at front
-void insertNodeTop(Node*& head, Node* newHead ){
-	Node* temp;
-	temp = head;
-	head = newHead;
-	head->next = temp;
-
-}
-
-void insertDataTop(Node *& head, int val){
-	Node* newNode = createNode(val);
-	Node* temp = new Node();
-	temp = head;
-	head = newNode;
-	head->next = temp;
-}
-void findNode(Node* n, int val){
-	int counter = 1;
+void findNode(Node *n, int val){
+	int count =1;
 	while(n->data != val){
-		n = n->next;
-		counter++;
-	}
-	cout<<"position: " << counter << endl;
-}
-
-void insertDataBottom(Node *& n, int val){
-	Node* temp = createNode(val);
-	while(n->next != NULL){
+		if(n == NULL){
+			cout<< "no find \n";
+		}
+		count++;
 		n = n->next;
 	}
-	n->next = temp;
-	return;
+	cout<<"Position of val " << val << " is: " << count <<endl;
 }
-
 
 void printList(Node* n){
 	while (n != NULL){
@@ -77,37 +54,113 @@ void printList(Node* n){
 	cout<<"\n";
 }
 
+void push(Node *& n, int val){
+	if(n == NULL){
+		n = createNode(val);
+		return;
+	}
+	Node* temp = n;
+	n = createNode(val);
+	n->next = temp;
+	//if n is empty
+}
+
+void append(Node *& n, int val){
+	if(n == NULL){
+		n = createNode(val);
+		return;
+	}
+	Node* newNode = createNode(val);
+	Node* temp = n;
+	while(temp->next != NULL){
+		temp = temp->next;
+	}
+	temp->next = newNode;
+}
+
+void reverse(Node *&n){
+	//create new list then push each one i go to, to new list
+	Node* revList= NULL;
+	Node* tempList = n;
+
+	while(tempList != NULL){
+		push(revList, tempList->data);
+		tempList = tempList->next;
+	}
+	
+	n = revList;
+}
+
+int findLength(Node *n){
+	int count = 0;
+	while(n != NULL){
+		count++;
+		n = n-> next;
+	}
+	return count;
+}
+
+void deleteKey(Node *&n, int val){
+	Node* newList = NULL;
+	Node *temp = n;
+
+	while(temp != NULL ) {
+		if(temp->data != val){
+			append(newList, temp->data);
+		}
+		temp = temp->next;
+	}
+	n = newList;
+}
+
+void deletePosition(Node *&n, int pos){
+	if(pos == 1){
+		Node* temp = n;
+		n = temp->next;
+		return
+	}
+	int count = pos;
+	if(pos > findLength(n)){
+		cout<<"list not long enough" << endl;
+	}
+	Node *temp = n;
+
+	while(temp->next != NULL){
+		count--;
+		//im gonna need to think about this, i cant go back
+		//i want to look at next but what about position 1?
+		if(count == 0){
+			
+		}
+		temp = temp->next;
+		
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	Node* head;
-	Node* second;
-	Node* third;
-
-	head = new Node();
-	second = new Node();
-	third = new Node();
-
+	Node* head = new Node();
 	head->data = 1;
+	head->next = NULL;
+
+	Node* second = createNode(2);
 	head->next = second;
 
-	second->data =2;
-	second->next = third;
+	push(head, 3);
 
-	third->data = 3;
-	third->next = NULL;
 
-	Node* fourth;
-	fourth = new Node();
-	fourth->data = 4;
-	fourth->next = NULL;
-
-	insertNodeTop(head, fourth);
-	insertDataTop(head, 5);
-	insertDataBottom(head, 6);
-
+	append(head, 4);
 	printList(head);
 
 	findNode(head,2);
+
+	reverse(head);
+	printList(head);
+
+	cout<< "length of list is: "<< findLength(head) << '\n';
+
+	deleteKey(head, 2);
+	printList(head);
 
 	return 0;
 }
